614e72f7248f5d83587194fdb053cba3cf78cc8a
Martin Wang <wangxuesong@gmail.com>
2023-10-10 09:37:35 +0800

    feat(file_view_controller.go): 添加对符号链接的处理 在文件视图控制器中，我们现在可以处理符号链接。如果链接指向的是目录，我们将递归地添加树节点。这个改变是为了提高我们的文件浏览器的功能性。
    Signed-off-by: Martin Wang <wangxuesong@gmail.com>

--- pkg/controllers/file_view_controller.go
+++ pkg/controllers/file_view_controller.go
@@ -30,12 +30,30 @@ func addTreeNode(target *tview.TreeNode, path string) {
 		panic(err)
 	}
 	for _, file := range files {
+		fullname := filepath.Join(path, file.Name())
 		node := tview.NewTreeNode(file.Name()).
-			SetReference(filepath.Join(path, file.Name()))
+			SetReference(fullname)
+		info, err := os.Lstat(fullname)
+
+		if err == nil {
+			if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+				// Handling symlinks. Note, may need to handle recursive symlinks with care to prevent infinite recursion
+				target, err := os.Readlink(fullname)
+				if err == nil {
+					info, err = os.Stat(target)
+				}
+				if err == nil && info.IsDir() {
+					node.SetColor(tcell.ColorBlue).
+						Collapse()
+					addTreeNode(node, fullname)
+				}
+			}
+
+		}
 		if file.IsDir() {
 			node.SetColor(tcell.ColorBlue).
 				Collapse()
-			addTreeNode(node, filepath.Join(path, file.Name()))
+			addTreeNode(node, fullname)
 		}
 		target.AddChild(node)
 	}
